52b1fe03a26b8955694e63fca207a978a6b353d9
yanam-dev <yamusjko@gmail.com>
2020-01-11 02:34:33 +0100

    added top header with slider and map

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,11 @@
 $(function(){
 
-
+    $('.header__top-slider').slick({
+        infinite: true,
+        fade: true,
+        autoplay: true,
+        speed: 800,
+        slidesToShow: 1
+    });
     
 });
