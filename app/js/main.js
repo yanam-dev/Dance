273d82bff24528dacca153ac59b672932c125fe6
yanam-dev <yamusjko@gmail.com>
2020-01-13 01:55:56 +0100

    added slogan section, nav menu with menu-btn adaptation, added socials link rotated  with orange bg

--- app/js/main.js
+++ app/js/main.js
@@ -7,5 +7,28 @@ $(function(){
         speed: 800,
         slidesToShow: 1
     });
+    // menu button
+    const navSLide = () => {
+        const menu__btn = document.querySelector('.menu__btn');
+        const nav = document.querySelector('.menu__list');
+        const navLinks = document.querySelectorAll('.menu__list li');
+        
+        menu__btn.addEventListener('click', () => {
+            // Toggle Nav
+            nav.classList.toggle('menu-active')
+    
+            // Animate Links
+            navLinks.forEach((link, index) => {
+                if (link.style.animation) {
+                    link.style.animation = '';
+                } else {
+                    link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+                }
+            });
+            // Burger ANimation
+            menu__btn.classList.toggle('toggle');
+        });
+    }
+    navSLide();
     
 });
